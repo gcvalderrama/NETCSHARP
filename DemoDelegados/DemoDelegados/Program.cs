fa43bcc17233d7ea2a485b805df49afa05bc80ef
Gregory Cesar Valderrama Vilca <gcvalderrama@hotmail.com>
2012-12-01 16:23:41 -0500

    Del Async Anonymous

--- DemoDelegados/DemoDelegados/Program.cs
+++ DemoDelegados/DemoDelegados/Program.cs
@@ -11,29 +11,23 @@ namespace DemoDelegados
     {
         static void Main(string[] args)
         {
-
             Func<string, string> cmdPrint = null;
 
-            cmdPrint += Print;
-
-            var cb = new AsyncCallback(OnTerminar);
-            var result = cmdPrint.BeginInvoke("Juan", cb, cmdPrint);
+            cmdPrint = new Func<string, string>(
+                    delegate(string mensaje) {
+                        Thread.Sleep(10000);
+                        return "Hola " + mensaje;
+                    }
+                );
+            var cb = new AsyncCallback(delegate(IAsyncResult result) {
+                var del = result.AsyncState as Func<string, string>;
+                var men = del.EndInvoke(result);
+                Console.WriteLine(men);
+            });
+            cmdPrint.BeginInvoke("Juan", cb, cmdPrint);
             Otra();            
             Console.ReadLine();
-        }
-        public static void OnTerminar(IAsyncResult result)
-        {
-            var del = result.AsyncState as Func<string, string>;
-            var men = del.EndInvoke(result);
-            Console.WriteLine(men);
-        }
-
-
-        public static string Print(string mensaje)
-        {
-            Thread.Sleep(10000);
-            return "Hola " + mensaje;
-        }
+        }          
         public static void Otra()
         {
             Console.WriteLine("Hacinedo otra tarea");
