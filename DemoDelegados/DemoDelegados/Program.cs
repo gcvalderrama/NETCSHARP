ca1e0a840fdf5df8abe5db4437bbd882be72a81b
Gregory Cesar Valderrama Vilca <gcvalderrama@hotmail.com>
2012-12-01 16:28:35 -0500

    del asyn lambda

--- DemoDelegados/DemoDelegados/Program.cs
+++ DemoDelegados/DemoDelegados/Program.cs
@@ -14,11 +14,11 @@ namespace DemoDelegados
             Func<string, string> cmdPrint = null;
 
             cmdPrint = new Func<string, string>(
-                    delegate(string mensaje) {
-                        Thread.Sleep(10000);
-                        return "Hola " + mensaje;
-                    }
-                );
+                    x=> { Thread.Sleep(10000); return  "Hola " + x;}
+            );
+
+                 
+
             var cb = new AsyncCallback(delegate(IAsyncResult result) {
                 var del = result.AsyncState as Func<string, string>;
                 var men = del.EndInvoke(result);
